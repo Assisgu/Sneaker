361f3a92cd37c87948efa5c22c9ad957e4719940
Assisgu <89754301+Assisgu@users.noreply.github.com>
2022-06-04 22:25:51 -0300

    Trabalhando no navbar e footer

--- resources/views/layouts/store.blade.php
+++ resources/views/layouts/store.blade.php
@@ -18,7 +18,7 @@
 <body>
     <header>
         {{-- bg-white --}}
-        <nav class="navbar navbar-expand-md navbar-light shadow-sm dino-navbar-menu"> 
+        <nav class="navbar navbar-expand-md navbar-light shadow-sm dino-navbar-menu "> 
             <div class="container">
                 <a class="navbar-brand logo" href="{{ url('/') }}">
                     <img src="./storage/imagem/logo.png" alt="Logo Dino Sneaker">
@@ -28,9 +28,9 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarCategoria" role="button" data-bs-toggle="dropdown">Categorias</a>
+                    <ul class="navbar-nav mr-auto ">
+                        <li class="nav-item dropdown ">
+                            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarCategoria" role="button" data-bs-toggle="dropdown">Categorias</a>
                             <ul class="dropdown-menu" aria-labelledby="navbarCategoria">
                                 @foreach (\App\Models\Brand::all() as $brand)
                                     <a class="dropdown-item" href="{{ route('search-brand', $brand->id) }}">{{ $brand->name }}</a>
@@ -38,7 +38,7 @@
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarTag" role="button" data-bs-toggle="dropdown">Tags</a>
+                            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarTag" role="button" data-bs-toggle="dropdown">Tags</a>
                             <ul class="dropdown-menu" aria-labelledby="navbarTag">
                                 @foreach (\App\Models\Tag::all() as $tag)
                                     <a class="dropdown-item" href="{{ route('search-tag', $tag->id) }}">{{ $tag->name }}</a>
@@ -47,19 +47,19 @@
                         </li>
                         @if(Auth()->user() && Auth()->user())
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('cart.index') }}">
+                            <a class="nav-link text-white" href="{{ route('cart.index') }}">
                                 Carrinho
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('order.index') }}">
+                            <a class="nav-link text-white" href="{{ route('order.index') }}">
                                 Ordens
                             </a>
                         </li>
                         @endif 
                         @if(Auth()->user() && Auth()->user()->isAdmin())
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('product.index') }}">
+                            <a class="nav-link text-white" href="{{ route('product.index') }}">
                                 Admin
                             </a>
                         </li>
@@ -68,7 +68,7 @@
 
                     <form action="{{ route('search.product') }}" class="form-group my-0 mx-auto w-50">
                         <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Digite o nome do produto" name="s">
+                            <input type="text" class="form-control" placeholder="O que procura ?" name="s">
                             <div class="input-group-append">
                                 <button type="submit" class="input-group-text">Buscar</button>
                             </div>
@@ -78,14 +78,14 @@
                     <ul class="navbar-nav ms-auto">
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">Login</a>
+                                <a class="nav-link text-white" href="{{ route('login') }}">Login</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">Registrar</a>
+                                <a class="nav-link text-white" href="{{ route('register') }}">Registrar</a>
                             </li>
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarUser" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a id="navbarUser" class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     {{ Auth::user()->name }}
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarUser">
@@ -118,25 +118,94 @@
         </section>
 
     </main>
-    <footer class="container p-5 bg-primary text-white">
-        <div class="row">
-            <div class="col-sm-8 mb-5">
-                <h2 class="h4">Localização:</h2>
-                <address>Rua Lorem, ipsum dolor, 386<br>
-                Lorem, ipsum - Lorem, LR<br>
-                CEP: 00000-000<br>
-                Telefone: (11) 8888-8888
-                </address>
-            </div>
-            <div class="col-sm-4">
-                <h2 class="h4">Horario de Funcionamento para loja fisica</h2>
-                <ul class="list-unstyled">
-                    <li>Segunda - Sexta: 9:00 - 20:00</li>
-                    <li>Sábado - 9:00 - 16:00</li>
-                </ul>
-            </div>
+    
+
+    <!-- Footer -->
+<footer class="text-center text-lg-start dino-footer text-muted">
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom container">
+      <div class="me-5 d-none d-lg-block font-footer text-white">
+        <span>Nos acompanhe também nas redes sociais:</span>
+      </div>
+
+      <div>
+        <a href="https://www.facebook.com/Dinosnkrs-100647968897097/" class="me-4 text-reset">
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" color="#fff" fill= "currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+          </svg>
+        </a>
+ 
+        <a href="https://www.instagram.com/accounts/login/?next=/dino.snkrs/" class="me-4 text-reset">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" color="#fff" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+                <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 
+                1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 
+                0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 
+                2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 
+                1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
+              </svg>
+        </a>
+      </div>
+    </section>
+  
+    <section class="">
+      <div class="container text-center text-md-start mt-5">
+        <div class="row mt-3">
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold mb-4 text-white"> <i class="me-3"> Dino Sneakers</i> </h6>
+            <p class="text-white">
+              Here you can use rows and columns to organize your footer content. Lorem ipsum
+              dolor sit amet, consectetur adipisicing elit.
+            </p>
+          </div>
+
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 text-white">
+            <h6 class="text-uppercase fw-bold mb-4"> Pagamento</h6>
+            <p> Pix</p>
+            <p> Crédito</p>
+            <p> Mercado pago</p>
+            <p> Boleto</p>
+          </div>
+
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 text-white">
+            <h6 class="text-uppercase fw-bold mb-4"> Meios de Envio </h6>
+                <p> Pix</p>
+                <p> Crédito</p>
+                <p> Mercado pago</p>
+                <p> Boleto</p>
+          </div>
+
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 text-white">
+            <h6 class="text-uppercase fw-bold mb-4"> Contato </h6>
+            <p> 
+                <svg   xmlns="http://www.w3.org/2000/svg" width="20" height="20" color="#fff" fill="currentColor" class="me-3 bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                    <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+                </svg>
+                São Paulo, SP - Brasil 
+            </p> 
+            <p> 
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" color="#fff" fill="currentColor" class="me-3 bi bi-envelope-check" viewBox="0 0 16 16">
+                    <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2H2Zm3.708 6.208L1 11.105V5.383l4.708 2.825ZM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2-7-4.2Z"/>
+                    <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Zm-1.993-1.679a.5.5 0 0 0-.686.172l-1.17 1.95-.547-.547a.5.5 0 0 0-.708.708l.774.773a.75.75 0 0 0 1.174-.144l1.335-2.226a.5.5 0 0 0-.172-.686Z"/>
+                </svg> 
+              suporte@dinosneakers.com.br 
+            </p>
+            <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" color="#fff" fill="currentColor" class="me-3 bi bi-telephone-fill" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                </svg>    
+                +55 (11) 97662-5433
+            </p>
+          </div>
         </div>
-    </footer>
+      </div>
+    </section>
+
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+      © 2022 Copyright:
+      <a class="text-reset fw-bold" href="https://mdbootstrap.com/">TSI-senac.com</a>
+    </div>
+
+  </footer>
 </body>
 
 </html>
